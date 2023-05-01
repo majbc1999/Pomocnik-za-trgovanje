4fadbdfe8c1feabf30c5330ddf81b2154161cf28
alanmarinic <marinic.alan@gmail.com>
2023-05-01 19:09:28 +0200

    Dodelani podatki
    Funkcija združi več csv datotek v eno

--- Podatki/get_history.py
+++ Podatki/get_history.py
@@ -1,9 +1,10 @@
 import csv
 import os
-from yahoofinancials import YahooFinancials
+import pandas as pd
+from yahoofinancials import YahooFinancials as yf
 
 
-trading_pairs = ['BTC-USD', 'ETH-USD', 'LINK-USD', 'XMR-USD', 'AAPL', 'SPY', 'NVDA', 'TSLA', 'EURUSD=X']
+trading_pairs = ['BTC-USD', 'ETH-USD', 'LINK-USD', 'XMR-USD', 'AAPL', 'SPY', 'NVDA', 'TSLA', 'EURUSD=X', 'MATIC-USD', 'PAXG-USD']
 
 begin_date = '2022-01-01'
 end_date = '2023-4-30'
@@ -26,7 +27,7 @@ def shorten_list(seznam):
     return sez
 
 def pripravi_imenik(ime_datoteke):
-    '''Če še ne obstaja, pripravi prazen imenik za dano datoteko.'''
+    '''Ce se ne obstaja, pripravi prazen imenik za dano datoteko.'''
     imenik = os.path.dirname(ime_datoteke)
     if imenik:
         os.makedirs(imenik, exist_ok=True)
@@ -42,13 +43,32 @@ def zapisi_csv(slovarji, imena_polj, ime_datoteke):
 
 def get_historic_data(seznam_parov):
     '''Za vsak simbol ustvari csv dokument, ter
-    v njega shrani zadnjo dnevno ceno za določeno časovno obdobje'''
+    v njega shrani zadnjo dnevno ceno za določeno casovno obdobje'''
     for simbol in seznam_parov:
-        zacasni_sez = YahooFinancials(simbol).get_historical_price_data( begin_date, end_date, 'daily')
+        zacasni_sez = yf(simbol).get_historical_price_data( begin_date, end_date, 'daily')
         seznam_cen = zacasni_sez[simbol]['prices']
         seznam_cen = shorten_list(seznam_cen)
         for i in seznam_cen:
-            i.update({"simbol": str(simbol)})
-        zapisi_csv(seznam_cen,  ['simbol', 'date', 'price'], 'Podatki/' + str(simbol) + '.csv')
+            i.update({"symbol_id": str(simbol)})
+        zapisi_csv(seznam_cen,  ['symbol_id', 'date', 'price'], 'Podatki/Posamezni_simboli/' + str(simbol) + '.csv')
+
+def get_symbols():
+    '''Vrne seznam csv datotek v mapi'''
+    sez_datotek = list()
+    for _, _, files in os.walk(r'Podatki/Posamezni_simboli'):
+        for file in files:
+            if (file.endswith('.csv')):
+                sez_datotek.append(file)
+    return sez_datotek
+
+def merge_csv(seznam, csv_name):
+    '''Zdruzi izbrane posamezne csv dokumente'''
+    zacasni = list()
+    for item in seznam:
+         zacasni.append(pd.read_csv(r'Podatki/Posamezni_simboli/' + str(item)))
+    df = pd.concat(zacasni, copy=False)
+    df.to_csv('Podatki/'+ str(csv_name), index=False)
+
 
 get_historic_data(trading_pairs)
+merge_csv(get_symbols(), 'price_history.csv')
