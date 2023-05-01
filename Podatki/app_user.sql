170f41805b469134ce5fa24c631b539ad77291da
Sara Žužek <sara.zuzek@gmail.com>
2023-05-01 17:29:27 +0200

    Dodala in popravila podatke

---
+++ Podatki/app_user.sql
@@ -0,0 +1,5 @@
+INSERT INTO app_user (id_user, name, surname, date_of_birth) VALUES (1, 'Alan', 'Marinić', '2000-03-27');
+INSERT INTO app_user (id_user, name, surname, date_of_birth) VALUES (2, 'Sara', 'Žužek', '2000-07-29');
+INSERT INTO app_user (id_user, name, surname, date_of_birth) VALUES (3, 'Nika', 'Novak', '1998-11-15');
+INSERT INTO app_user (id_user, name, surname, date_of_birth) VALUES (4, 'David', 'Novak', '1975-02-22');
+
---
+++ Podatki/pair.sql
@@ -0,0 +1,9 @@
+INSERT INTO pair (symbol, name) VALUES ('BTC-USD', 'Bitcoin');
+INSERT INTO pair (symbol, name) VALUES ('ETH-USD', 'Ethereum');
+INSERT INTO pair (symbol, name) VALUES ('XMR-USD', 'Monero');
+INSERT INTO pair (symbol, name) VALUES ('AAPL', 'Apple');
+INSERT INTO pair (symbol, name) VALUES ('LINK-USD', 'Chainlink');
+INSERT INTO pair (symbol, name) VALUES ('EURUSD', 'Euro');
+INSERT INTO pair (symbol, name) VALUES ('SPY', 'S&P 500');
+INSERT INTO pair (symbol, name) VALUES ('TSLA', 'Tesla');
+INSERT INTO pair (symbol, name) VALUES ('NVDA', 'NVIDIA Corp');
