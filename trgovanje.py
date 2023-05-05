7503ec5d1e7c9dbe681740d5a6154e0cba6453d4
Sara Žužek <sara.zuzek@gmail.com>
2023-05-05 13:29:48 +0200

    Dodala views in testni strežnik

--- trgovanje.py
+++ trgovanje.py
@@ -25,19 +25,62 @@ cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 # odkomentiraj, če želiš sporočila o napakah
 # debug(True)
 
-#@get('/static/<filename:path>')
-#def static(filename):
-#    return static_file(filename, root='static')
+@get('/static/<filename:path>')
+def static(filename):
+   return static_file(filename, root='static')
+
+@get('/')
+def zacetna_stran():
+    cur.execute("""
+      SELECT symbol,name from pair
+   """)
+    return template('zacetna_stran.html', pair=cur)
+
+@get('/prijava')
+def prijava():
+    return template('prijava.html')
 
 @get('/users')
 def users():
     cur.execute("SELECT name, surname, date_of_birth FROM app_user")
     return template('users.html', app_user=cur)
 
+@get('/add_user')
+def add_user():
+    return template('add_user.html', name='', surname='', date_of_birth='', napaka=None)
+
+@post('/add_user')
+def add_user_post():
+    name = request.forms.name
+    surname = request.forms.surname
+    date_of_birth = request.forms.date_of_birth
+    try:
+        cur.execute("INSERT INTO app_user (name, surname, date_of_birth) VALUES (%s, %s, %s) RETURNING id_user",
+                    (name, surname, date_of_birth))
+        conn.commit()
+    except Exception as ex:
+        conn.rollback()
+        return template('add_user.html', name=name, surname=surname, date_of_birth=date_of_birth,
+                        napaka='Zgodila se je napaka: %s' % ex)
+    redirect(url('/users'))
+
 @get('/trades')
-def users():
+def trades():
     cur.execute("SELECT symbol_id, type, strategy, RR, target, date, duration, TP, PNL FROM trade")
     return template('trades.html', trade=cur)
+
+@get('/pairs')
+def pairs():
+    cur.execute("SELECT symbol, name FROM pair")
+    return template('pairs.html', pair=cur)
+
+@get('/asset')
+def asset():
+    cur.execute("""SELECT app_user.name, symbol_id, amount FROM asset 
+    INNER JOIN app_user ON user_id = id_user 
+    ORDER BY app_user.name """)
+    return template('asset.html', asset=cur)
+
 
 
 ######################################################################
